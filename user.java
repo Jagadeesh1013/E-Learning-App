b65193235112a883ddd52adda9b2e0c963141e83
Jagadeesh <jagadeeshkanchana105@gmail.com>
2024-07-08 22:13:09 +0530

    Removed Unwanted Lines

--- user.java
+++ user.java
@@ -2,13 +2,13 @@ class {
 
    public static void main (String [] args ){
 
-<<<<<<< Updated upstream
+
 
 // Bug - 131 fixing completed.
-=======
+
 // Bug - 121 fixing 
 Method - 1
->>>>>>> Stashed changes
+
      
 
      System.out.println ("Hello");
